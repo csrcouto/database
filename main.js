e3d2946b72b490c109f2646c94db71f028ef6eaf
Cesar Couto <csrcouto@gmail.com>
2021-01-01 17:26:31 -0300

    criação dos métodos execute e createtable

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-const statement = "create table author (id number, name string, age number, city string, state string, country string)";
-const regexp = /create table ([a-z]+) \((.+)\)/;
-const parsedStatement = statement.match(regexp);
-const tableName = parsedStatement[1];
-let columns = parsedStatement[2].split(", ");
 const database = {
-    tables: {
-        [tableName]: {
-            columns: {
-
-            },
+    tables: {},
+    createTable(statement) {
+        const regexp = /create table ([a-z]+) \((.+)\)/;
+        const parsedStatement = statement.match(regexp);
+        const tableName = parsedStatement[1];
+        this.tables[tableName] = {
+            columns: {},
             data: []
         }
+        let columns = parsedStatement[2].split(", ");
+        for (let column of columns) {
+            column = column.split(" ");
+            const name = column[0];
+            const type = column[1];
+            this.tables[tableName].columns[name] = type;
+        }
     },
+    execute(statement) {
+        if (statement.startsWith("create table")) {
+            return this.createTable(statement);
+        }
+    }
 };
-for (let column of columns) {
-    column = column.split(" ");
-    const name = column[0];
-    const type = column[1];
-    database.tables[tableName].columns[name] = type;
-}
+database.execute("create table author (id number, name string, age number, city string, state string, country string)");
 console.log(JSON.stringify(database, undefined, " "));
