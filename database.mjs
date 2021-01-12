b0dbf713d7f60a27106d0af33e6883dd3122abe4
Cesar Couto <csrcouto@gmail.com>
2021-01-11 21:25:41 -0300

    divisão do projeto em módulos

--- database.mjs
+++ database.mjs
@@ -1,30 +1,6 @@
-class DatabaseError {
-    constructor(statement, message) {
-        this.statement = statement;
-        this.message = message;
-    }
-}
-class Parser {
-    constructor() {
-        this.commands = new Map();
-        this.commands.set("createTable", /create table ([a-z]+) \((.+)\)/);
-        this.commands.set("insert", /insert into ([a-z]+) \((.+)\) values \((.+)\)/);
-        this.commands.set("select", /select (.+) from ([a-z]+)(?: where (.+))?/);
-        this.commands.set("delete", /delete from ([a-z]+)(?: where (.+))?/);    
-    }
-    parse(statement) {
-        for (let [command, regExp] of this.commands) {
-            const parsedStatement = statement.match(regExp);
-            if (parsedStatement) {
-                return {
-                    command,
-                    parsedStatement
-                }
-            }
-        }
-    }
-}
-class Database {
+import {Parser} from "./parser.mjs";
+import {DatabaseError} from "./databaseError.mjs";
+export class Database {
     constructor() {
         this.tables = {};
         this.parser = new Parser();
@@ -92,15 +68,4 @@ class Database {
         const message = `Syntax error: "${statement}"`;
         throw new DatabaseError(statement, message);
     }
-}
-try {
-    const database = new Database();
-    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
-    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
-    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
-    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
-    database.execute("delete from author where id = 2");
-    console.log(JSON.stringify(database.execute("select name, age from author"), undefined, " "));
-} catch (e) {
-    console.log(e.message);
 }
